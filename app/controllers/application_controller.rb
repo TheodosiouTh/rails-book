1e524d0f6fff4fcb6a92b210f53e64379a1650bd
ThanosTheo <thanosthd@gmail.com>
2020-04-20 14:40:09 +0300

    Added Sing IN/UP, and FriendRequest model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :require_login
+
+
+
+    protected
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name,:last_name, :email, :password)}
+
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
+        end
+
+    private
+        def require_login
+            if current_user.nil? && !['/users/sign_up', '/users/sign_in'].include?(request.fullpath)
+                redirect_to '/users/sign_in'
+            end
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  root to: "users#index"
+
+  devise_scope :user do
+    get "/signup", to: "devise/registrations#new"
+    get "/login", to: "devise/sessions#new"
+    get "/logout", to: "devise/sessions#destroy"
+  end
 end
